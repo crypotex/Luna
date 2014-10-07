ea494700755403cdbdd68883ba1b4bf9f9604f8b
Andre Tättar <b02166@ut.ee>
2014-10-07 12:22:54 +0300

    Project Start , Home Assignment #3- breakpoint : Class Intro is used as an ANT target and this class should run your IntroUI in the MAIN method.

---
+++ POS/src/ee/ut/math/tvt/Luna/Intro.java
@@ -0,0 +1,5 @@
+package ee.ut.math.tvt.Luna;
+
+public class Intro {
+
+}
---
+++ POS/src/ee/ut/math/tvt/Luna/IntroUI.java
@@ -0,0 +1,14 @@
+package ee.ut.math.tvt.Luna;
+/*
+ * TODO: Add LogoUrl / logo and add softwareVersion
+ */
+public class IntroUI {
+	private String teamName = "Luna";
+	private String teamLeader = "Andre";
+	private String teamLeaderEmail = "b02166@ut.ee";
+	private String[] teamMembers = {"Annika Laumets", "Viktoria Plemakova",
+			"Henri Rästas"};
+	private String LogoUrl = "";
+	private String softwareVersion = "";
+	
+}
