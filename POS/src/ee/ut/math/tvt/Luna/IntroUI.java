056404da220bf19edbdc5b71eaa6b98fcb950ad7
ViktoriaP <viktoria.plemakova@gmail.com>
2014-10-10 22:48:38 +0300

    ANT targets, added "dist" and "jrun"

--- POS/src/ee/ut/math/tvt/Luna/IntroUI.java
+++ POS/src/ee/ut/math/tvt/Luna/IntroUI.java
@@ -7,7 +7,7 @@ public class IntroUI {
 	private String teamLeader = "Andre";
 	private String teamLeaderEmail = "b02166@ut.ee";
 	private String[] teamMembers = {"Annika Laumets", "Viktoria Plemakova",
-			"Henri Rastas"}; //talle ei sobinud buildi ajal Henri nimes ä täht, nii et panin selle hetkel a'ks.
+			"Henri Rastas"}; //tapitahed ei sobi, isegi kommentaarides
 	private String LogoUrl = "";
 	private String softwareVersion = "";
 	
