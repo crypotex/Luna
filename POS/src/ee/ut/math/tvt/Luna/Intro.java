c267f5a065e6e1b031ece7eef0cc3e255bcab36b
ViktoriaP <viktoria.plemakova@gmail.com>
2014-10-13 16:26:56 +0300

    A lot of stuff

--- POS/src/ee/ut/math/tvt/Luna/Intro.java
+++ POS/src/ee/ut/math/tvt/Luna/Intro.java
@@ -17,73 +17,13 @@ import javafx.stage.Stage;
 
 import org.apache.log4j.Logger;
 
-public class Intro extends Application {
-	
-	private static final Logger log = Logger.getLogger(Intro.class);
-		
-		public static void main(String[] args) throws IOException {
+public class Intro extends IntroUI {
+
+	public static void main(String[] args) throws IOException {
+		try {
 			launch(args);
-			
-			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		
-		
-
-		public void start(Stage primaryStage) {
-			
-
-			// Deal with the properties to hashmap shit
-			IntroUI props = new IntroUI();
-			LinkedProperties applicationProperties = props.getApplicationProperties();
-			LinkedProperties versionProperties = props.getVersionProperties();
-			
-			LinkedHashMap<String,String> map = new LinkedHashMap<String,String>();
-			
-	        for (Object name: applicationProperties.keySet()) {
-	            map.put(name.toString(), applicationProperties.getProperty(name));
-	        }
-	        
-	       
-			
-			// Start with the boring graphic shit
-	        BorderPane juur = new BorderPane();
-	        
-	        GridPane keskmine = new GridPane();
-	        keskmine.setHgap(20);
-	        keskmine.setVgap(10);
-	        keskmine.setPadding(new Insets(0, 10, 0, 10));
-	        
-	        int counter = 0;
-	        for (String key: map.keySet()) {
-	        	if (key.equals("Logo_url")) {
-	        		Image img = new Image(map.get(key));
-	        		ImageView imgView = new ImageView(img);
-	                imgView.setFitWidth(300);
-	                imgView.setPreserveRatio(true);
-	                imgView.setSmooth(true);
-	                imgView.setCache(true);
-	        		keskmine.add(imgView, 0, counter, 2, 1);
-	        		keskmine.setAlignment(Pos.CENTER);
-	        		GridPane.setHalignment(imgView, HPos.CENTER); //paneb logo keskele
-	        		counter += 1;
-	        	} 
-	        	else {
-	        		Text keyOutput = new Text(key.replace('_', ' '));
-	            	Text keyValue = new Text(map.get(key));
-	            	keskmine.add(keyOutput, 0, counter);
-	            	keskmine.add(keyValue, 1, counter);
-	            	counter += 1;
-	        	}
-			}
-	        
-	        juur.setCenter(keskmine);
-	        
-	        Scene appScene = new Scene(juur,500,500); 
-	        primaryStage.setTitle("Luna Properties");
-	        primaryStage.setScene(appScene);
-	        primaryStage.show();
-	        log.info("Window opened!");
-	        
-		}
-		  
+	}  
 }
--- POS/src/ee/ut/math/tvt/Luna/IntroUI.java
+++ POS/src/ee/ut/math/tvt/Luna/IntroUI.java
@@ -3,26 +3,38 @@ package ee.ut.math.tvt.Luna;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import org.apache.log4j.Logger;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /*
  * TODO: Add LogoUrl / logo and add softwareVersion
  */
 
 
-public class IntroUI {
-	
-	
-	
-	
+public class IntroUI extends Application {
+	private final Logger log = Logger.getLogger(Intro.class);
 	private LinkedProperties applicationProperties = new LinkedProperties();
 	private LinkedProperties versionProperties = new LinkedProperties();
-	
+
 	public IntroUI() {
 		this.applicationProperties = getApplicationPropertiesFromFile();
 		this.versionProperties = getVersionPropertiesFromFile();
 	}
-	
-	
+
+
 	public LinkedProperties getApplicationProperties() {
 		return applicationProperties;
 	}
@@ -44,7 +56,7 @@ public class IntroUI {
 			throw new RuntimeException("Method : getApplicationProperties has failed ! ");
 		}
 	}
-	
+
 	private LinkedProperties getVersionPropertiesFromFile(){
 		LinkedProperties prop = new LinkedProperties();
 		String propFileName = "version.properties";
@@ -53,9 +65,9 @@ public class IntroUI {
 			prop.load(input);
 			FileOutputStream output = new FileOutputStream(propFileName);
 			//prop.setProperty("build.revision.number", 
-					//Integer.toString(Integer.parseInt(prop.getProperty("build.revision.number"))+1) );
+			//Integer.toString(Integer.parseInt(prop.getProperty("build.revision.number"))+1) );
 			//prop.setProperty("build.number", ( prop.getProperty("build.major.number") + "." +
-					//prop.getProperty("build.minor.number") + "." + prop.getProperty("build.revision.number") ));
+			//prop.getProperty("build.minor.number") + "." + prop.getProperty("build.revision.number") ));
 			prop.store(output, null);
 			input.close();
 			output.close();
@@ -64,7 +76,64 @@ public class IntroUI {
 		} catch (IOException e) {
 			throw new RuntimeException("Method : getVersionProperties has failed ! ");
 		}
-		
+
 	}
-	
+
+	public void start(Stage primaryStage) {
+
+		// Deal with the properties to hashmap shit
+		LinkedProperties applicationProperties = getApplicationProperties();
+		LinkedProperties versionProperties = getVersionProperties();
+
+		LinkedHashMap<String,String> map = new LinkedHashMap<String,String>();
+
+		for (Object name: applicationProperties.keySet()) {
+			map.put(name.toString(), applicationProperties.getProperty(name));
+		}
+
+
+
+		// Start with the boring graphic shit
+		BorderPane juur = new BorderPane();
+
+		GridPane keskmine = new GridPane();
+		keskmine.setHgap(20);
+		keskmine.setVgap(10);
+		keskmine.setPadding(new Insets(0, 10, 0, 10));
+
+		int counter = 0;
+		for (String key: map.keySet()) {
+			if (key.equals("Logo_url")) {
+				try {
+					Image img = new Image(map.get(key));
+					ImageView imgView = new ImageView(img);
+					imgView.setFitWidth(300);
+					imgView.setPreserveRatio(true);
+					imgView.setSmooth(true);
+					imgView.setCache(true);
+					keskmine.add(imgView, 0, counter, 2, 1);
+					keskmine.setAlignment(Pos.CENTER);
+					GridPane.setHalignment(imgView, HPos.CENTER); //paneb logo keskele
+					counter += 1;
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			} 
+			else {
+				Text keyOutput = new Text(key.replace('_', ' '));
+				Text keyValue = new Text(map.get(key));
+				keskmine.add(keyOutput, 0, counter);
+				keskmine.add(keyValue, 1, counter);
+				counter += 1;
+			}
+		}
+
+		juur.setCenter(keskmine);
+
+		Scene appScene = new Scene(juur,500,500); 
+		primaryStage.setTitle("Luna Properties");
+		primaryStage.setScene(appScene);
+		primaryStage.show();
+		log.info("Window opened!");
+	}	
 }
