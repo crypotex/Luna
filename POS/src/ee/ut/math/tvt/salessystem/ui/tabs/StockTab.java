83ec8572f826c3ad445c67628cb23cf4931d5774
Annika Laumets <laumets.annika@gmail.com>
2014-10-21 23:51:07 +0300

    The window for adding new stock works.

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/StockTab.java
@@ -2,10 +2,14 @@ package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -16,6 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.table.JTableHeader;
 
 import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
@@ -69,36 +74,56 @@ public class StockTab {
 		addItem = new JButton("Add");
 		addItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(e.getSource().toString());
-				JDialog jf2 = new JDialog(new JFrame(), "Jeee");
-				JTextField id = new JTextField();
-				JTextField quantity = new JTextField("1");
-				JTextField name = new JTextField();
-				JTextField price = new JTextField();
-				jf2.add(new JLabel("Bar code:"));
-				jf2.add(id);
-				jf2.add(new JLabel("Amount:"));
-				jf2.add(quantity);
-				jf2.add(new JLabel("Name:"));
-				jf2.add(name);
-				jf2.add(new JLabel("Price:"));
-				jf2.add(price);
-				
-				jf2.setSize(300, 300);
-				jf2.setVisible(true);
-				jf2.pack();
+				JDialog adding_window = new JDialog(new JFrame(), "Adding a new item to stock");
+				GridBagLayout gb2 = new GridBagLayout();
+			    GridBagConstraints gc2 = new GridBagConstraints();
+			    gc2.ipadx = 10;
+			    gc2.ipady = 10;
+			    adding_window.setLayout(gb2);
+			    
 
+			    //lisame valjad id, nime, koguse ja hinna jaoks
+			    JTextField id = new JTextField(5);
+				JTextField quantity = new JTextField("1", 5);
+				JTextField name = new JTextField(5);
+				JTextField price = new JTextField(5);
+				
+			    ArrayList<JTextField> textfields = new ArrayList<>();
+			    textfields.addAll(Arrays.asList(id, quantity, name, price));
+				
+				ArrayList<String> labels = new ArrayList<>();
+				labels.addAll(Arrays.asList("Bar code:", "Amount:", "Name:", "Price:"));
+				int counter = 0;
+				for (int i = 0; i < labels.size(); i++) {
+					gc2.gridx = 0;
+					gc2.gridy = counter;
+					adding_window.add(new JLabel(labels.get(i)), gc2);
+					gc2.gridx = 1;
+					adding_window.add(textfields.get(i), gc2);
+					counter += 1;
+				}
+				
+
+				//lisame nupu, mis lisab uue toote stocki
 				JButton addItemButton = new JButton("Add to stock");
 				addItemButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						//StockTableModel.this.addItem();;
-						System.out.println("Added to stock");
+						System.out.println("Added to stock"); //siin peab tegema veel nii, et ta lisaks stocki
 					}
 				});
-				//siin peab stockitemi lisamise meetodi valja kutsuma
-
+				
+				gc2.gridx = 0;
+				gc2.gridy = 4;
+				gc2.gridwidth = 2;	
+				adding_window.add(addItemButton, gc2);
+				
+				adding_window.setMinimumSize(new Dimension(200, 200));
+				adding_window.setLocation(550, 300);
+				adding_window.setVisible(true);
 			}
 		});
+		
 		gc.gridwidth = GridBagConstraints.RELATIVE;
 		gc.weightx = 1.0;
 		panel.add(addItem, gc);
