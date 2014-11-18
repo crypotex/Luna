868b1d159bc8551d493383dda4a2d598b0433180
ViktoriaP <viktoria.plemakova@gmail.com>
2014-11-18 13:20:26 +0200

    Hibernate & test error

--- POS/test/StockTableModeltest.java
+++ POS/test/StockTableModeltest.java
@@ -10,36 +10,23 @@ import org.junit.Test;
 import ee.ut.math.tvt.salessystem.ui.model.StockTableModel;
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
-import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
-
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class StockTableModeltest {
 
-    	private SalesDomainController controller;
-    	private SalesSystemModel model;
-    	private StockTableModel stockModel;
-    	private static StockItem stockItem1;
+        SalesDomainController SDC = new SalesDomainControllerImpl();
+        StockTableModel model = new StockTableModel();
+        List<StockItem> stock;
 
         @Before
         public void setUp() {
-            stockItem1 = new StockItem ((long) 99, "OMA PRODUKT", "oma kirjeldus",99.00, 3);
-            controller = new SalesDomainControllerImpl();
-            model = new SalesSystemModel(controller);
-            stockModel = model.getWarehouseTableModel();
+                stock = SDC.loadWarehouseState();
+                model.populateWithData(stock);
         }
 
         @Test
-        public void testValidateNameUniqueness(){
-                long initialItemCount= (long) stockModel.getRowCount();
-                
-                stockModel.addItem(stockItem1); //Add an item once
-                assertEquals((long)3, stockModel.getItemById(99).getQuantity()); 
-                
-                stockModel.addItem(stockItem1); //Add the same item again
-                assertEquals((long)6, stockModel.getItemById(99).getQuantity()); //After adding the duplicate, the qty should have doubled
-
-                //after adding the same item twice, we should have ended with only 1 new row in the table 
-                assertEquals(initialItemCount+1, (long)stockModel.getRowCount());
-                }
+        public void testGetItemByIdWhenItemExists() {
+                StockItem item = stock.get(0);
+                assertEquals(item, model.getItemById(item.getId()));
+        }
 }
